c4fc06bbf37c8b7a3d51bd5918a064235e9e27ed
jazdosanjh <jas@dosanjh.co.uk>
2019-05-30 19:57:23 +0100

    Created new build for deployment

---
+++ build/js.edcdfce9.js
@@ -0,0 +1,8 @@
+parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcelRequire,u="function"==typeof require&&require;function f(t,n){if(!r[t]){if(!e[t]){var i="function"==typeof parcelRequire&&parcelRequire;if(!n&&i)return i(t,!0);if(o)return o(t,!0);if(u&&"string"==typeof t)return u(t);var c=new Error("Cannot find module '"+t+"'");throw c.code="MODULE_NOT_FOUND",c}p.resolve=function(r){return e[t][1][r]||r},p.cache={};var l=r[t]=new f.Module(t);e[t][0].call(l.exports,p,l,l.exports,this)}return r[t].exports;function p(e){return f(p.resolve(e))}}f.isParcelRequire=!0,f.Module=function(e){this.id=e,this.bundle=f,this.exports={}},f.modules=e,f.cache=r,f.parent=o,f.register=function(r,t){e[r]=[function(e,r){r.exports=t},{}]};for(var c=0;c<t.length;c++)try{f(t[c])}catch(e){i||(i=e)}if(t.length){var l=f(t[t.length-1]);"object"==typeof exports&&"undefined"!=typeof module?module.exports=l:"function"==typeof define&&define.amd?define(function(){return l}):n&&(this[n]=l)}if(parcelRequire=f,i)throw i;return f}({"DOAq":[function(require,module,exports) {
+
+},{}],"clu1":[function(require,module,exports) {
+
+},{"../../node_modules/normalize.css/normalize.css":"DOAq"}],"QvaY":[function(require,module,exports) {
+"use strict";require("../sass/main.scss");var t=document.querySelectorAll(".tab-item"),e=document.querySelectorAll(".tab-content-item");function c(t){s(),o(),this.classList.add("tab-border"),document.querySelector("#".concat(this.id,"-content")).classList.add("show")}function s(){t.forEach(function(t){t.classList.remove("tab-border")})}function o(){e.forEach(function(t){t.classList.remove("show")})}t.forEach(function(t){t.addEventListener("click",c)});
+},{"../sass/main.scss":"clu1"}]},{},["QvaY"], null)
+//# sourceMappingURL=/js.edcdfce9.js.map
